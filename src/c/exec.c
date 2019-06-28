158a5b196047c9521247daa9d1628cd2d758b3ef
aafsari <amirafsari99@yahoo.com>
2019-06-28 13:40:33 +0430

    Testbenches created

--- src/c/exec.c
+++ src/c/exec.c
@@ -1,12 +1,34 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include"echohash.c"
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
+
 int main(int argc, char const *argv[])
 {
-    char* input = "input";
-    char* hash = malloc(500);
+    uint16_t inp = 1;
+    void* input = &inp;
+    void* hash = malloc(500);
     
     echohash(hash, input);
-    printf("%s", hash );
+    printBits(strlen(hash) , hash);
+    printf("\n");
+    printBits(strlen(input) , input);
 }
+
 
